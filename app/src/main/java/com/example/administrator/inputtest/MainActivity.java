cc1e596a15ed34af6de195b3a34b1baad5f14b06
ws <805058027@qq.com>
2017-06-07 15:00:51 +0800

    增加了注释

--- app/src/main/java/com/example/administrator/inputtest/MainActivity.java
+++ app/src/main/java/com/example/administrator/inputtest/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.administrator.inputtest;
 
+import android.content.Context;
+import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 
 import com.example.administrator.inputtest.listener.CustomGlobalLayoutListener;
 
+/**
+ * 注意：软键盘的弹出需要在主配置文件里面配置参数
+ */
 public class MainActivity extends AppCompatActivity {
 
     private ScrollView mScrollView;
@@ -22,9 +29,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initID();
-        customGlobalLayoutListener = new CustomGlobalLayoutListener(this, mScrollView, rootView,
-                mEditText);
-        mScrollView.getViewTreeObserver().addOnGlobalLayoutListener(customGlobalLayoutListener);
+        initSetting();
     }
 
     /**
@@ -34,6 +39,15 @@ public class MainActivity extends AppCompatActivity {
         mScrollView = (ScrollView) findViewById(R.id.scrollView);
         rootView = (LinearLayout) findViewById(R.id.rootView);
         mEditText = (EditText) findViewById(R.id.report_Comment);
+    }
+
+    /**
+     * 软键盘弹出和输入框的光标设置
+     */
+    private void initSetting() {
+        customGlobalLayoutListener = new CustomGlobalLayoutListener(this, mScrollView, rootView,
+                mEditText);
+        mScrollView.getViewTreeObserver().addOnGlobalLayoutListener(customGlobalLayoutListener);
         // 编辑框设置触摸监听
         mEditText.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -46,38 +60,43 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    //控制输入框光标的显示隐藏
-       /* reportEditParent.getViewTreeObserver().addOnGlobalLayoutListener(
+    /**
+     * 控制输入框光标的显示隐藏（在此demo中没有使用,可以自行测试）
+     *
+     * @param parentRoot EditText的父布局
+     * @param mEditText  EditText本身
+     */
+    private void mEditCursor(final View parentRoot, final EditText mEditText) {
+        parentRoot.getViewTreeObserver().addOnGlobalLayoutListener(
                 new ViewTreeObserver.OnGlobalLayoutListener() {
                     @Override
                     public void onGlobalLayout() {
                         Rect r = new Rect();
-                        reportEditParent.getWindowVisibleDisplayFrame(r);
-                        int screenHeight = reportEditParent.getRootView()
-                                .getHeight();
+                        parentRoot.getWindowVisibleDisplayFrame(r);
+                        int screenHeight = parentRoot.getRootView().getHeight();
                         int heightDifference = screenHeight - (r.bottom);
                         if (heightDifference > 200) {
                             //软键盘显示
-                            reportComment.setFocusable(true);
-                            reportComment.setFocusableInTouchMode(true);
-                            reportComment.requestFocus();
-                            reportComment.findFocus();
+                            mEditText.setFocusable(true);
+                            mEditText.setFocusableInTouchMode(true);
+                            mEditText.requestFocus();
+                            mEditText.findFocus();
                             InputMethodManager imm = (InputMethodManager) getSystemService
                                     (Context.INPUT_METHOD_SERVICE);
-                            imm.showSoftInput(reportComment, InputMethodManager.SHOW_FORCED);//
+                            imm.showSoftInput(mEditText, InputMethodManager.SHOW_FORCED);
                         } else {
-                            reportComment.clearFocus();
+                            mEditText.clearFocus();
                         }
                     }
                 });
 
-        reportComment.setOnTouchListener(new View.OnTouchListener() {
+        mEditText.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                reportComment.setFocusable(true);
-                reportComment.setFocusableInTouchMode(true);
+                mEditText.setFocusable(true);
+                mEditText.setFocusableInTouchMode(true);
                 return false;
             }
-        });*/
-
+        });
+    }
 }
