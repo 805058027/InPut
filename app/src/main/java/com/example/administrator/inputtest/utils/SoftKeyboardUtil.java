42056093a84949279babff774e7d7e333ba7e79b
805058027 <535382832@qq.com>
2018-04-10 09:34:34 +0800

    两个键盘相关的工具类

---
+++ app/src/main/java/com/example/administrator/inputtest/utils/SoftKeyboardUtil.java
@@ -0,0 +1,42 @@
+package com.example.administrator.inputtest.utils;
+
+import android.app.Activity;
+import android.graphics.Rect;
+import android.view.View;
+import android.view.ViewTreeObserver;
+
+/**
+ * 获取键盘的高度和显示状态
+ * Created by Administrator on 2017/8/31.
+ */
+
+public class SoftKeyboardUtil {
+    public static void observeSoftKeyboard(Activity activity, final OnSoftKeyboardChangeListener
+            listener) {
+        final View decorView = activity.getWindow().getDecorView();
+        decorView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver
+                .OnGlobalLayoutListener() {
+            int previousKeyboardHeight = -1;
+
+            @Override
+            public void onGlobalLayout() {
+                Rect rect = new Rect();
+                decorView.getWindowVisibleDisplayFrame(rect);
+                int displayHeight = rect.bottom - rect.top;
+                int height = decorView.getHeight();
+                int keyboardHeight = height - displayHeight;
+                if (previousKeyboardHeight != keyboardHeight) {
+                    boolean hide = (double) displayHeight / height > 0.8;
+                    listener.onSoftKeyBoardChange(keyboardHeight, !hide);
+                }
+
+                previousKeyboardHeight = height;
+
+            }
+        });
+    }
+
+    public interface OnSoftKeyboardChangeListener {
+        void onSoftKeyBoardChange(int softKeybardHeight, boolean visible);
+    }
+}
